5139b47761a71c1a3fad0fe9dce0ddd170f44bc3
gogojom <artemy00797@yandex.ru>
2020-03-03 11:25:11 +0300

    Added FHPersonalDataBase - DB, which stores FHDay information for each day.

--- FHDataBase.py
+++ FHDataBase.py
@@ -2,16 +2,6 @@ class FHDataBase:
     def __init__(self):
         self.database = {}
 
-class FHProductsDataBase(FHDataBase):
-    def __init__(self):
-        super().__init__()
-        # { product_name + '_' + product_company : calories }
-    
-    def __new__(self):
-        if not hasattr(self, 'instance'):
-            self.instance = super(FHProductsDataBase, self).__new__(self)
-        return self.instance
-
     def __getitem__(self, key):
         if key in self.database:
             return self.database[key]
@@ -20,7 +10,23 @@ class FHProductsDataBase(FHDataBase):
     def __setitem__(self, key, value):
         self.database[key] = value
 
+class FHProductsDataBase(FHDataBase):
+    def __new__(self):
+        if not hasattr(self, 'instance'):
+            self.instance = super(FHProductsDataBase, self).__new__(self)
+        return self.instance
+
     def GetKey(self, a_ProductName, a_ProductCompany=None):
         if a_ProductCompany is not None:
             return a_ProductName + '_' + a_ProductCompany
         return a_ProductName
+
+class FHPersonalDataBase(FHDataBase):
+    def __new__(self):
+        if not hasattr(self, 'instance'):
+            self.instance = super(FHDataBase, self).__new__(self)
+        return self.instance
+
+    def PrintInfo(self, a_Date):
+        print('====== {} ======'.format(a_Date))
+        print(self[a_Date])
