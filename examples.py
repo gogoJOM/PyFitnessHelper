5c092a13cd5020bfa6d1cd3b921786b3159cbb5c
gogojom <artemy00797@yandex.ru>
2020-03-03 11:11:29 +0300

    Added examples.

--- examples.py
+++ examples.py
@@ -1,8 +1,10 @@
 from FHDataBase import FHProductsDataBase
+from utils import FHProduct, FHDay
 
 def ProductsDBExample_SimpleCompanyProduct(a_DataBase):
     product_name = "gr"
     product_company = "Mi"
+
     key = a_DataBase.GetKey(product_name, product_company)
 
     calories = a_DataBase[key]
@@ -25,9 +27,33 @@ def ProductsDBExample_SimpleProduct(a_DataBase):
     
     print(key, a_DataBase[key])
 
+def DayExample(a_DataBase):
+    day = FHDay("03.03.20", a_DataBase)
+
+    breakfast = FHProduct("egg", None, 50)
+    day.AddBreakfast(breakfast)
+
+    first_snack = FHProduct("choco_bar", "Milka", 200)
+    day.AddSnack(first_snack)
+
+    lunch = FHProduct("salad", None, 100)
+    day.AddLunch(lunch)
+    lunch = FHProduct("Beef", None, 500)
+    day.AddLunch(lunch)
+
+    second_snack = FHProduct("jogurt", "Danone", 125)
+    day.AddSnack(second_snack)
+
+    dinner = FHProduct("steak", "Miratorg", 550)
+    day.AddDinner(dinner)
+
+    print(day)
+
 if __name__ == "__main__":
     DataBase = FHProductsDataBase()
     ProductsDBExample_SimpleCompanyProduct(DataBase)
     ProductsDBExample_SimpleProduct(DataBase)
+    DayExample(DataBase)
     print()
+
     
