6852e6ccb08f555da55ff04221d8fb7528a58bfb
gogojom <artemy00797@yandex.ru>
2020-03-05 19:45:29 +0300

    Added print for FHPersonalDataBase. Reverted Singletone pattern - now DataBases are module-global.

--- FHDataBase.py
+++ FHDataBase.py
@@ -10,23 +10,22 @@ class FHDataBase:
     def __setitem__(self, key, value):
         self.database[key] = value
 
-class FHProductsDataBase(FHDataBase):
-    def __new__(self):
-        if not hasattr(self, 'instance'):
-            self.instance = super(FHProductsDataBase, self).__new__(self)
-        return self.instance
+    def __contains__(self, item):
+        return item in self.database
 
+class FHProductsDataBase(FHDataBase):
     def GetKey(self, a_ProductName, a_ProductCompany=None):
         if a_ProductCompany is not None:
             return a_ProductName + '_' + a_ProductCompany
         return a_ProductName
 
 class FHPersonalDataBase(FHDataBase):
-    def __new__(self):
-        if not hasattr(self, 'instance'):
-            self.instance = super(FHDataBase, self).__new__(self)
-        return self.instance
-
     def PrintInfo(self, a_Date):
         print('====== {} ======'.format(a_Date))
         print(self[a_Date])
+
+    def __str__(self):
+        return str(self.database)
+
+gProductDatabase = FHProductsDataBase()
+gPersonalDatabase = FHPersonalDataBase()
