bfb31378be5ef1d0b394eb780afd3048f2b28cab
gogoJOM <artemy00797@yandex.ru>
2020-05-09 13:19:16 +0300

    Added setup.py and requirements.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as readme:
+    description = readme.read()
+
+with open('requirements.txt', "r", encoding="utf8") as requirements:
+    requires = requirements.read().splitlines()
+
+setuptools.setup(
+    name="PyFitnessHelper",
+    version="0.0.1",
+    author="Artemii Zakharov, Ekaterina Safronova",
+    author_email="artemy00797@yandex.ru, katyasafit@gmail.com",
+    description="Helper to manage your calories intake",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gogoJOM/PyFitnessHelper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requires,
+    python_requires='>=3.7',
+)
