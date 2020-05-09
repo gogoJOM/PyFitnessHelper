4a50331afb13b2a46af5bf3907efea6961da7de8
gogoJOM <artemy00797@yandex.ru>
2020-05-09 13:41:54 +0300

    Updated forgotten package finder.

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setuptools.setup(
     long_description=description,
     long_description_content_type="text/markdown",
     url="https://github.com/gogoJOM/PyFitnessHelper",
+	packages=setuptools.find_packages(
+        exclude=["tests.py"]
+    ),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
